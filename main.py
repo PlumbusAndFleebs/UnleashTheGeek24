0ff2f23d3ca39ab6cf04c513853a0a090b7af0e3
Christopher SCHMALZ <cschmalz@muclrnd1038.muc.amadeus.net>
2024-11-15 12:49:57 +0100

    final push 2.0

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 import math
 from typing import Optional
 
+
+
 class Entity:
 
     def __init__(self, entity):
@@ -62,6 +64,10 @@ class ProteinStock:
         self.c = my_c
         self.d = my_d
 
+
+    def can_create_tentacle(self):
+        return self.b > 0 and self.c > 0
+
 class Map:
     def __init__(self, width, height):
         # Initialize a 2D list with None as placeholder values, allowing Entity objects later
@@ -96,6 +102,190 @@ class Map:
         # Debugging output
         #print(f"Entities found of type '{type}': {[e._type for e in entities]}")
         return entities
+
+
+    def is_tentacle_set(self):
+        tentacles = self.get_all_entities_by_type(type="TENTACLE")
+        owner_organs = self.get_all_entities_by_owner(1)
+
+        for tentacle in tentacles:
+            for organ in owner_organs:
+                if tentacle.organ_id == organ.organ_id:
+                    return True 
+        return False
+
+
+    def get_close_to_enemy(self, distance):
+        enemies = self.get_all_entities_by_owner(0)
+        ours = self.get_all_entities_by_owner(1)
+        
+        candidates = []
+
+        for our in ours:
+            for enemy in enemies:
+                if Entity.get_distance(our, enemy) <= distance:
+
+                    candidates.append((our, enemy))
+
+        # returns tuples!!!!
+        return candidates
+
+
+    def get_closest_to_root(self, entities, root_owner_id):
+        if len(entities) == 0:
+            return None
+        
+        roots = map.get_all_entities_by_type("ROOT")
+        root = None
+        for i in roots:
+            if i.owner == root_owner_id:
+               root = i 
+        
+        if root == None:
+           return root
+
+        closest = entities[0]
+        closest_distance = 1000000
+        
+        for entity in entities:
+            dist = Entity.get_distance(entity[0], root)
+            if dist < closest_distance:
+                closest_distance = dist
+                closest = entity
+# this is a tuple!!!
+        return closest
+
+
+    def get_special_move(self, organ_target_pair, my_protein_stock, organ_type, place_dist):
+        if organ_target_pair == None:
+            return None
+        organ, target = organ_target_pair
+
+       
+
+        dist = Entity.get_distance(organ, target)
+        if ((organ_type == "HARVESTER") and not self.can_create_harvester(my_protein_stock)) or ((organ_type == "TENTACLE") and not my_protein_stock.can_create_tentacle() ):
+
+            print(f"line 166: entered", file=sys.stderr, flush=True)
+
+            return f"GROW {organ.organ_id} {target.x} {target.y} BASIC"
+
+       
+    
+        if dist == place_dist:
+            # Horizontal cases (either left or right)
+            if organ.y - target.y == 0:
+                # target is right from organ
+                if target.x - organ.x > 0:
+                    if self.is_field_empty(organ.x + 1, organ.y):
+                        return f"GROW {organ.organ_id} {organ.x + 1} {organ.y} {organ_type} E"
+                    else:
+                        cords = organ.get_free_neighbor_coords(self)
+                        if cords == None:
+                            self.default_move
+                        else: 
+                            return f"GROW {organ.organ_id} {cords[0]} {cords[1]} BASIC" 
+                # target is left from organ
+                elif target.x - organ.x < 0:
+                    if self.is_field_empty(organ.x - 1, organ.y):
+                        return f"GROW {organ.organ_id} {organ.x - 1} {organ.y} {organ_type} W"
+                    else:
+                        cords = organ.get_free_neighbor_coords(self)
+                        if cords == None:
+                            self.default_move
+                        else: 
+                            return f"GROW {organ.organ_id} {cords[0]} {cords[1]} BASIC" 
+            # Vertical cases (either up or down)
+            elif organ.x - target.x == 0:
+                # target is under the organ
+                if target.y - organ.y > 0:
+                    if self.is_field_empty(organ.x, organ.y + 1):
+                        return f"GROW {organ.organ_id} {organ.x} {organ.y + 1} {organ_type} S"
+                    else:
+                        cords = organ.get_free_neighbor_coords(self)
+                        if cords == None:
+                            self.default_move
+                        else: 
+                            return f"GROW {organ.organ_id} {cords[0]} {cords[1]} BASIC"                    
+                    
+                # target is over the organ
+                elif target.y - organ.y < 0:
+                    if self.is_field_empty(organ.x, organ.y - 1):
+                        return f"GROW {organ.organ_id} {organ.x} {organ.y - 1} {organ_type} N"
+                    else:
+                        cords = organ.get_free_neighbor_coords(self)
+                        if cords == None:
+                            self.default_move
+                        else: 
+                            return f"GROW {organ.organ_id} {cords[0]} {cords[1]} BASIC"      
+                    
+                    
+                    #return f"GROW {organ.organ_id} {organ.x} {organ.y - 1} {organ_type} N"
+
+        elif dist < place_dist and dist > 1:
+            #target ist links vom organ
+            if organ.x - target.x > 0:
+                #nach oben
+                if organ.y - target.y > 0:
+                    if self.is_field_empty(organ.x-1, organ.y):
+                        return f"GROW {organ.organ_id} {organ.x-1} {organ.y} {organ_type} N"
+                    else:
+                        cords = organ.get_free_neighbor_coords(self)
+                        if cords == None:
+                            self.default_move
+                        else: 
+                            return f"GROW {organ.organ_id} {cords[0]} {cords[1]} BASIC"     
+                    
+                    
+                    #return f"GROW {organ.organ_id} {organ.x-1} {organ.y} {organ_type} N"
+                #nach unten
+                else:
+
+
+                    if self.is_field_empty(organ.x-1, organ.y):
+                        return f"GROW {organ.organ_id} {organ.x-1} {organ.y} {organ_type} S"
+                    else:
+                        cords = organ.get_free_neighbor_coords(self)
+                        if cords == None:
+                            self.default_move
+                        else: 
+                            return f"GROW {organ.organ_id} {cords[0]} {cords[1]} BASIC"  
+                    #return f"GROW {organ.organ_id} {organ.x-1} {organ.y} {organ_type} S"
+            #target ist rechts vom organ
+            if organ.x - target.x < 0:
+                #nach oben
+                if organ.y - target.y > 0: 
+                    
+                    if self.is_field_empty(organ.x+1, organ.y):
+                        return f"GROW {organ.organ_id} {organ.x+1} {organ.y} {organ_type} N"
+                    else:
+                        cords = organ.get_free_neighbor_coords(self)
+                        if cords == None:
+                            self.default_move
+                        else: 
+                            return f"GROW {organ.organ_id} {cords[0]} {cords[1]} BASIC"  
+                    
+                    #return f"GROW {organ.organ_id} {organ.x+1} {organ.y} {organ_type} N"
+                else: 
+                    
+                    if self.is_field_empty(organ.x+1, organ.y):
+                        return f"GROW {organ.organ_id} {organ.x+1} {organ.y} {organ_type} S"
+                    else:
+                        cords = organ.get_free_neighbor_coords(self)
+                        if cords == None:
+                            self.default_move
+                        else: 
+                            return f"GROW {organ.organ_id} {cords[0]} {cords[1]} BASIC"  
+                    
+                    
+                    
+                    #return f"GROW {organ.organ_id} {organ.x+1} {organ.y} {organ_type} S"
+    #Basic
+        print(f"line 207: entered", file=sys.stderr, flush=True)
+        return f"GROW {organ.organ_id} {target.x} {target.y} BASIC"
+
+
+
 
     def can_create_harvester(self, my_protein_stock):
         if my_protein_stock.c < 1 and my_protein_stock.d < 1:
@@ -150,7 +340,9 @@ class Map:
                 if organ.y - protein.y == 0:
                     # Protein is right from organ
                     if protein.x - organ.x > 0:
+                        
                         return f"GROW {organ.organ_id} {organ.x + 1} {organ.y} HARVESTER E"
+                         
                     # Protein is left from organ
                     elif protein.x - organ.x < 0:
                         return f"GROW {organ.organ_id} {organ.x - 1} {organ.y} HARVESTER W"
@@ -181,6 +373,57 @@ class Map:
                         return f"GROW {organ.organ_id} {organ.x+1} {organ.y} HARVESTER S"
         #Basic
         return f"GROW {organ.organ_id} {protein.x} {protein.y} BASIC"
+
+    
+    def is_field_empty(self, x, y):
+        if self.map[x][y] == None:
+            return True
+        return False
+    
+    
+    
+    
+    def tentacle_move(self, my_organs_close_to_enemy, protein_stock):
+        
+        if len(my_organs_close_to_enemy) == 0:
+
+            #wachse zu Root
+            print("Line 308: move to enemy rootcase", file=sys.stderr, flush=True)
+            #TODO take out tentacle already exist case
+            my_organs = self.get_all_entities_by_owner(1)
+            my_organs_as_tuple = []
+
+            for organ in my_organs:
+                my_organs_as_tuple.append((organ, self.getEnemyRoot()))
+
+           
+            
+            organ_target_pair  = self.get_closest_to_root(my_organs_as_tuple, 0)
+            print( f"Line 316: {organ_target_pair}", file=sys.stderr, flush=True)
+            if organ_target_pair == None:
+                print("Line 311: move to enemy rootcase (organ_target_pair is None)", file=sys.stderr, flush=True)
+                return self.default_move()
+            return f"GROW {organ_target_pair[0].organ_id} {organ_target_pair[1].x} {organ_target_pair[1].y} BASIC"
+
+        else:
+            ## Erstelle Tentacle 
+            print("Line 316: mach mal tentacle", file=sys.stderr, flush=True)
+            organ_target_pair = self.get_closest_to_root(my_organs_close_to_enemy, 1)
+            return self.get_special_move(organ_target_pair, protein_stock, "TENTACLE", 3)            
+
+
+    def getEnemyRoot(self):
+        enemies = self.get_all_entities_by_owner(0)
+
+        for enemy in enemies:
+            if enemy._type == "ROOT":
+                return enemy
+            
+        return None
+
+
+
+
 
     def default_move(self):
         owners = self.get_all_entities_by_owner(1)
@@ -251,11 +494,25 @@ while True:
 
         owner, protein = map.get_nearest_owner_protein_pair()
         command = "HELP"
+        my_organs_close_to_enemy = map.get_close_to_enemy(3)
+
         # Case: No more protein :(
-        if owner == None or protein == None or owner._type == "HARVESTER":
-            command = map.default_move()
-        else:
+        if not owner == None and not protein == None and not owner._type == "HARVESTER" and (len(my_organs_close_to_enemy)==0):
+            print("protein case", file=sys.stderr, flush=True)
             command = map.get_next_move(map.get_nearest_owner_protein_pair(), my_protein_stock)
+        elif my_protein_stock.can_create_tentacle():
+            print("tentacle case", file=sys.stderr, flush=True)
+            command = map.tentacle_move(my_organs_close_to_enemy, my_protein_stock)
+        else:
+            print("DEFAULT case", file=sys.stderr, flush=True)
+            command = map.default_move()
+
+        if command == None:
+            command = map.default_move()
+        # if owner == None or protein == None or owner._type == "HARVESTER":
+        #     command = map.default_move()
+        # else:
+        #     command = map.get_next_move(map.get_nearest_owner_protein_pair(), my_protein_stock)
         
         
 
